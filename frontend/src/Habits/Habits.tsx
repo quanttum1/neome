1c10112ed4eece4c2b52b4c6b1580d1b09b1a97d
Taras Tuz (aka Quantie) <tarastuz@proton.me>
2026-01-29 21:46:34 +0000

    Show habits properly

--- frontend/src/Habits/Habits.tsx
+++ frontend/src/Habits/Habits.tsx
@@ -1,17 +1,41 @@
 import useNeomeStore from '../useNeomeStore';
 import NewButton from '../NewButton';
+import { Link } from 'react-router';
 
 function Habits() {
   let habits = useNeomeStore(s => s.getState().habits);
 
-  return <div>
-    {/* TODO(2026-01-25 21:24:41): show habits the sane way */}
-    {habits.map(i => {
-      return JSON.stringify(i);
-    })} 
+  return (
+    <div className="pt-2 pb-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+        {habits.map((habit) => (
+          <div
+            className="text-[24px] pt-2 pb-3 pl-4 pr-4 rounded-[1.5vw] bg-neome-grey flex items-center gap-2"
+          >
+
+            <Link 
+              className="group flex cursor-pointer flex-col" to={`/habits/${habit.id}`}
+            >
+              <span className="group-hover:underline">{habit.name}</span>
+              {/* TODO(2026-01-29 21:23:08): show days of week */}
+              <span className="text-gray-400 text-[15px] italic">
+                {habit.daysOfWeek == 255 
+                  ? "Everyday" 
+                  : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"].filter((label, index) => 
+                    (habit.daysOfWeek >> index) % 2).join(" ,")}
+              </span>
+            </Link>
+            <div className="ml-auto flex gap-2 text-[1.5em]">
+              <span className="text-[#00FF00]">{habit.reward}</span>
+              <span className="text-[#FF0000]">{habit.penalty}</span>
+            </div>
+          </div>
+        ))}
+      </div>
 
     <NewButton />
-  </div>;
+    </div>
+  );
 }
 
 export default Habits;
