2771f19e1fc57b7522f65922c8ebebaa60c712b5
Taras Tuz (aka Quantie) <tarastuz@proton.me>
2026-02-24 20:57:52 +0000

    add a button to complete the task on the task card

--- frontend/src/Tasks/TaskCard.tsx
+++ frontend/src/Tasks/TaskCard.tsx
@@ -1,8 +1,11 @@
 import useNeomeStore from '../useNeomeStore';
-import Pin from '../assets/icons/pin.svg';
-import PinActive from '../assets/icons/pin_active.svg';
+import { useNavigate } from 'react-router';
+import PinIcon from '../assets/icons/pin.svg';
+import PinActiveIcon from '../assets/icons/pin_active.svg';
+import BinIcon from '../assets/icons/bin.svg';
+import CompleteIcon from '../assets/icons/complete.svg';
 import { Link } from 'react-router';
-import { now } from "../utc";
+import { now } from '../utc';
 
 interface TaskCardProps {
   task: Task;
@@ -10,24 +13,34 @@ interface TaskCardProps {
 
 function TaskCard(props: TaskCardProps) {
   let task = props.task;
+
   const taskTogglePinned = useNeomeStore((s) => s.taskTogglePinned);
+  const completeTask = useNeomeStore(s => s.completeTask);
+  const navigate = useNavigate();
+
+  function complete() {
+    completeTask(task.id);
+    navigate('/tasks/completed', {state: { reward: task.reward }});
+  }
+
+  const buttonClass = "hover:bg-neome-light-grey cursor-pointer rounded-[20%]";
 
   return (
     <div
-      className="text-[24px] pt-2 pb-3 pl-4 pr-4 rounded-[1.5vw] bg-neome-grey flex items-center gap-2"
+      className="group/card text-[24px] pt-2 pb-3 pl-4 pr-4 rounded-[1.5vw] bg-neome-grey flex items-center gap-2"
     >
 
       <div
         className="hover:bg-neome-light-grey hover:rounded-[50%] cursor-pointer p-1"
         onClick={() => taskTogglePinned(task.id)}
       >
-        <img src={task.isPinned ? PinActive : Pin}></img>
+        <img src={task.isPinned ? PinActiveIcon : PinIcon}></img>
       </div>
 
       <Link 
-        className="group flex cursor-pointer flex-col" to={`/tasks/${task.id}`}
+        className="group/link flex cursor-pointer flex-col" to={`/tasks/${task.id}`}
       >
-        <span className="group-hover:underline">{task.name}</span>
+        <span className="group-hover/link:underline">{task.name}</span>
         <span className="text-gray-400 text-[15px] italic">
           {(() => {
             const ms = new Date(task.deadline).getTime() - new Date(now()).getTime();
@@ -48,10 +61,24 @@ function TaskCard(props: TaskCardProps) {
           })()} left
         </span>
       </Link>
-      <div className="ml-auto flex gap-2 text-[1.5em]">
+
+      <div className="group-hover/card:hidden ml-auto flex gap-2 text-[1.5em]">
         <span className="text-[#00FF00]">{task.reward}</span>
         <span className="text-[#FF0000]">{task.penalty}</span>
       </div>
+
+      <div className="hidden group-hover/card:flex ml-auto flex gap-2 text-[1.5em]">
+        {/* TODO(2026-02-24 20:54:44): ask for comfirmation before completing the task */}
+        <button className={buttonClass} onClick={complete}>
+          <img src={CompleteIcon} />
+        </button>
+
+        {/* TODO(2026-02-24 20:53:26): add deletion button on task cards */}
+        {/* <button className={buttonClass}> */}
+        {/*   <img src={BinIcon} /> */}
+        {/* </button> */}
+      </div>
+
     </div>
   );
 }
